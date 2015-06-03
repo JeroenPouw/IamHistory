50cb3ec3b0331bf3625e7869d8d1cbf24cb9480e
JeroenPouw <pouwje@gmail.com>
2015-06-03 12:01:49 +0200

    Starting Commit
    Base scene for first prototype

---
+++ Assets/Scripts/MoviePlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoviePlay : MonoBehaviour {
+	public AudioSource audio1;
+	// Use this for initialization
+	void Start () {
+		//((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			
+			Renderer r = GetComponent<Renderer>();
+			MovieTexture movie = (MovieTexture)r.material.mainTexture;
+			
+			if (movie.isPlaying) {
+				movie.Pause();
+				audio1.Pause();
+
+			}
+			else {
+				movie.Play();
+				audio1.Play();
+			}
+		}
+	}
+}
